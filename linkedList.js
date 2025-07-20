a78246c25b585f84956aeccf90bcda0e3d65cb34
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-07-20 16:25:17 -0300

    start work on LinkedList class, add append func

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,26 @@
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.list = {};
+  }
+
+  // append to the end of the list
+  append(value, list = this.list) {
+    // create a node from the value
+    const newNode = new Node(value);
+    // if the object is empty append it
+    if (Object.keys(list).length === 0) {
+      list.value = newNode.value;
+      list.next = newNode.nextNode;
+    }
+    // if the next value is null append it
+    if (list.next === null) {
+      list.next = { value: newNode.value, next: newNode.nextNode };
+    }
+
+    // if the next value is not null we recursively call the function
+    append(value, list.next);
+  }
+}
 
 class Node {
   constructor() {
